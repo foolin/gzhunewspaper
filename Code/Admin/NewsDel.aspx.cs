4090e4c4b251d0804fecd717cf93cfbf428eeec7
zhushgo@msn.com <zhushgo@msn.com@c5dcce08-4e8a-11de-aa6a-4703752c21e7>
2009-06-09 09:31:14 +0000

    添加了新闻删除

--- Code/Admin/NewsDel.aspx.cs
+++ Code/Admin/NewsDel.aspx.cs
@@ -8,11 +8,25 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Web.UI.WebControls.WebParts;
 using System.Web.UI.HtmlControls;
+using Myweb.NewsPaper;
+using Studio.Web;
 
 public partial class Admin_NewsDel : AdminBase
 {
     protected void Page_Load(object sender, EventArgs e)
     {
 
+    }
+    protected override void OnPreRender(EventArgs e)
+    {
+        if (QS("id") == "" || !WebAgent.IsInt32(QS("id")))
+            WebAgent.AlertAndBack("参数错误");
+        News paper = new NewsAgent().GetNewsInfo(int.Parse(QS("id")));
+        if (paper == null)
+            WebAgent.AlertAndBack("新闻不存在");
+        if (new NewsAgent().DeleteNews(int.Parse(QS("id"))) > 0)
+            WebAgent.SuccAndGo("删除期刊成功", "NewsList.aspx");
+        else
+            WebAgent.AlertAndBack("删除期刊失败");
     }
 }
