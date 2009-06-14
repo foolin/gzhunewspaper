ed7d5fa99e230ef452cc0a48c8f216f889bb5158
liufuling@msn.com <liufuling@msn.com@c5dcce08-4e8a-11de-aa6a-4703752c21e7>
2009-06-14 06:12:54 +0000

    搜索模块

---
+++ Code/Ajax/Search.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Search.aspx.cs" Inherits="Ajax_Search" %>
+
+<div style="text-align:center;">
+    <asp:label runat="server" id="Tips" ForeColor="Red" text=""></asp:label>
+</div>
+
+<ul class="list">
+<asp:Repeater ID="ListNews" runat="server">
+    <ItemTemplate>
+        <li><a href="#" onclick="window.location.href='#NewsID=<%#Eval("NewsID")%>';LoadData();return false; ">
+            <%#Eval("Title").ToString().Replace(QS("Keyword"), "<font color=\"red\">" + QS("Keyword") + "</font>") %></a></li>
+    </ItemTemplate>
+</asp:Repeater>
+</ul>
+
+
+<div class="pagebar">
+    [第<%=page.GetPageIndex() %>页/共<%=page.GetPageCount() %>页] 
+    <a href="#" onclick="Search('<%=QS("Keyword")%>','<%=page.GetFirst() %>');return false; ">首页</a>
+    <a href="#" onclick="Search('<%=QS("Keyword")%>','<%=page.GetPre() %>');return false; ">上一页</a>
+     <a href="#" onclick="Search('<%=QS("Keyword")%>','<%=page.GetNext() %>');return false; ">下一页</a>
+     <a href="#" onclick="Search('<%=QS("Keyword")%>','<%=page.GetLast() %>');return false; ">尾页</a>
+</div>
---
+++ Code/Ajax/Search.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Collections;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using Myweb.NewsPaper;
+using Studio.Web;
+
+public partial class Ajax_Search : SiteBase
+{
+    public Pager page = new Pager();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected override void OnLoad(EventArgs e)
+    {
+        if (QS("Page") != "" && WebAgent.IsInt32(QS("Page")))
+            page.SetPageIndex(int.Parse(QS("Page")));
+        if (QS("Keyword") != "")
+        {
+            int Record = 0;
+            ListNews.DataSource = new NewsAgent().GetNewsList(QS("Keyword"), page.GetPageSize(), page.GetPageIndex(), out Record);
+            ListNews.DataBind();
+            page.SetTotalCount(Record);
+            if (Record == 0)
+                Tips.Text = "对不起，未能找到[" + QS("Keyword") + "]的记录！";
+        }
+        else
+        {
+            Tips.Text = "出错了，你未输入任何关键字！";
+        }
+
+        Response.Cache.SetExpires(DateTime.Now);
+        Response.Cache.AppendCacheExtension("no-cache");
+
+    }
+
+}
+
+
